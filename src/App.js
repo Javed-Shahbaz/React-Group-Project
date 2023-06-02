19f2a52eafb442962235ccf62789a34ba33724b9
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-06-02 18:39:08 +0500

    update files

--- src/App.js
+++ src/App.js
@@ -12,25 +12,11 @@ function App() {
         <NavLink />
         <Routes>
           <Route path="/rockets" element={<Rockets />} />
+
           <Route path="/" element={<Navigate to="/rockets" />} />
         </Routes>
       </BrowserRouter>
     </>
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavLink from './components/NavLink';
-import Missions from './components/pages/Mission/Missions';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <NavLink />
-      <Routes>
-        <Route path="/Missions" element={<Missions />} />
-      </Routes>
-    </BrowserRouter>
-
   );
 }
 export default App;
