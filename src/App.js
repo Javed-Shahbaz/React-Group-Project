4f51989f6127f858280af7b79daa300fb24a020b
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-06-03 03:27:50 +0500

    update files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
+import Rockets from './components/Rocket/Rocket';
 import NavLink from './components/NavLink';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavLink />
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavLink />
+        <Routes>
+          <Route path="/Rockets" element={<Rockets />} />
+          <Route path="/" element={<Navigate to="/Rockets" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
