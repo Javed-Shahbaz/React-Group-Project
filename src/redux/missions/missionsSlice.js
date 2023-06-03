f1dad7eb0f6b562e8c58b5477a182a29d6e9cbe6
Adeyemi Olusola <rphillipse@gmail.com>
2023-06-03 04:50:22 -0600

    Delete src directory

--- src/redux/missions/missionsSlice.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const MissionsSlices = createSlice({
-  name: 'Missions',
-  initialState: [],
-  reducers: {},
-});
-
-export default MissionsSlices.reducer;
