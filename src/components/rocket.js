ca16de4f343e92ee6b441df32997e745b2446464
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-06-03 19:14:26 +0500

    create rocket.css

--- src/components/rocket.js
+++ src/components/rocket.js
@@ -1,7 +1,48 @@
-const Rockets = () => (
-  <div>
-    Rockets
-  </div>
-);
+import { useEffect, React } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { cancelReserved, fetchData, reservedRocket } from '../../Redux/Rockets/RocketSlice';
+import './Rocket.css';
+
+function Rockets() {
+  const dispatch = useDispatch();
+  const { data, status } = useSelector((state) => state.rockets);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  return (
+    <div className="rocket-container">
+      {status && <div>Loading...</div>}
+      {data && data.map((rocket) => (
+        <div key={rocket.id} className="main-container">
+          <img
+            className="rocket-img"
+            src={rocket.flickr_images}
+            alt={rocket.rocket_name}
+          />
+          <div className="rocket-content">
+            <h3 className="header">{rocket.rocket_name}</h3>
+            <p className="description">
+              {rocket.reserved && (
+                <span className="reserve-markChanged">Reserved</span>
+              )}
+              {rocket.description}
+            </p>
+            {rocket.reserved ? (
+              <button type="submit" onClick={() => dispatch(cancelReserved(rocket.id))} className="cancel-reserved">Cancel Reservation</button>
+            ) : (
+              <button
+                type="submit"
+                className="reserved"
+                onClick={() => dispatch(reservedRocket(rocket.id))}
+              >
+                Reserve Rocket
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Rockets;
