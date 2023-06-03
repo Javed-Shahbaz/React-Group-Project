0faf6f4e270aeaa0dd865abc3888078d9cfef484
emarkees <rphillipse@gmail.com>
2023-06-03 05:55:58 -0600

    Add API files

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,59 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
-  missionData: [
-    {
-      id: 0,
-      title: 'Thaicom',
-      Description: '...',
-      status: 'NOT A MEMBER',
-    },
-    {
-      id: 1,
-      title: 'Thaicom',
-      Description: '...',
-      status: 'NOT A MEMBER',
-    },
-    {
-      id: 2,
-      title: 'Thaicom',
-      Description: '...',
-      status: 'NOT A MEMBER',
-    },
-    {
-      id: 3,
-      title: 'Thaicom',
-      Description: '...',
-      status: 'NOT A MEMBER',
-    },
-    {
-      id: 4,
-      title: 'Thaicom',
-      Description: '...',
-      status: 'NOT A MEMBER',
-    },
-  ],
+  missions: [],
+  loading: 'idle',
 };
 
+export const fetchMissions = createAsyncThunk('missions/fetchMissions', async () => {
+  try {
+    const response = await axios.get(API);
+    const missionWithId = response.data.map((missions) => ({
+      mission_id: missions.mission_id,
+      mission_name: missions.mission_name,
+      description: missions.description,
+      status: 'NOT A MEMBER',
+    }));
+    return missionWithId;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
 const missionsSlice = createSlice({
-  name: 'missionData',
+  name: 'missions',
   initialState,
   reducers: {
-    addMission: (state, action) => {
-      state.missionData.push(action.payload);
-    },
-    removeMission: (state, action) => {
-      state.missionData = state.missionData.filter((missionData) => missionData.id !== action.payload);
-    },
     updateMissionStatus: (state, action) => {
       const { missionId, status } = action.payload;
-      const mission = state.missionData.find((missionData) => missionData.id === missionId);
+      const mission = state.missions.find((missions) => missions.mission_id === missionId);
       if (mission) {
         mission.status = status;
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.missions = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state) => {
+        state.status = 'rejected';
+      });
+  },
 });
 
-export const { addMission, removeMission, updateMissionStatus } = missionsSlice.actions;
+export const { updateMissionStatus } = missionsSlice.actions;
 export default missionsSlice.reducer;
